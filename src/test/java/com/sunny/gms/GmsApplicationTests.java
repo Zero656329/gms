31aa90447576bfb5ddeed071a7b99100b0555d3c
Zero656329 <hwj656329@163.com>
2020-10-02 22:35:31 +0800

    first commit

---
+++ src/test/java/com/sunny/gms/GmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.gms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
